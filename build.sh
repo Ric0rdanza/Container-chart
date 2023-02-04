9723c2bbb10d8b349c3c1b874386ab4c8c9c08be
Ric0rdanza <1031293005@qq.com>
2023-02-04 02:59:49 +0100

    fix typo

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+docker buildx build --platform linux/amd64,linux/arm64 --push -t odonataprime/auth:gke backend/auth/
+
+docker buildx build --platform linux/amd64,linux/arm64 --push -t odonataprime/shortener:gke backend/main/
+
+docker buildx build --platform linux/amd64,linux/arm64 --push -t odonataprime/server:gke server
+
+
+
 helm dependency update containerization/
 
 helm package containerization/
