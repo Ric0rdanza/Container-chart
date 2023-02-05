79593b9959bfacece9f205cf7e181aa808c048b4
Ric0rdanza <1031293005@qq.com>
2023-02-05 16:36:58 +0100

    test

--- backend/main/main.py
+++ backend/main/main.py
@@ -46,7 +46,6 @@ def get_shorturl(long_url):
 # ownership: {username, shorturl}
 
 def db_create_shorturl(username, originalurl, shorturl):
-	print("Creating shorturl...", flush=True)
 	conn = db_connect()
 	cur = conn.cursor()
 	cur.execute("SELECT * FROM urls WHERE originalurl = \'%s\'" % (originalurl))
